b2aa6c636f65304fc9e6851914d69b055ff0292e
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-10 07:06:07 -0500

    Make directions simpler and more specific
    Clarify what each CRUD method should do in a success or failure case,
     and indicate that this is how promises will need to be used.
    Rename ArtMovement to Movement to avoid any surprises from file naming.

--- diagnostic.js
+++ diagnostic.js
@@ -11,14 +11,15 @@
 // art movements; the API for this part of the system must be standardized so
 // so that other parts of the application can use it.
 
-// Each ArtMovement needs to have the following features:
+// Each Movement needs to have the following features:
 // - A name (required).
-// - A description (no longer than 100 words).
-// - Start and end years (as numbers) for when the movement was most prominent.
+// - A description (no longer than 200 characters).
+// - Starting and ending years (as strings) for the time when the movement
+//    was most prominent.
 // - An 'ancestor', another art movement that this movement descends from.
 
-// Please implement the CRUD functions below, using Promises.
-// This is *NOT* Express code, so don't worry about doing anything beyond CRUD.
+// Please implement the CRUD functions below, using Promises to handle the
+//  specified success and failure behaviors.
 // Your Mongoose model should go in a separate file, in the `models` directory.
 
 /// STARTER CODE - DO NOT ALTER!
@@ -29,11 +30,21 @@ mongoose.connect('mongodb://localhost/mongoose-crud');
 /// ADD YOUR CODE BELOW
 
 const create = (name, description, startYear, endYear) => {};
+// Success -> Print new Movement as JSON
+// Failure -> Console.error
 
 const index = () => {};
+// Success -> Print all Movements as JSON
+// Failure -> Console.error
 
 const show = (id) => {};
+// Success -> Print specified Movement as JSON
+// Failure -> Console.error
 
 const update = (id, field, value) => {};
+// Success -> Print updated Movement as JSON
+// Failure -> Console.error
 
 const destroy = (id) => {};
+// Success -> Print 'removed'
+// Failure -> Console.error
